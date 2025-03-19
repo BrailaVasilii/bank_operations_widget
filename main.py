064ebaab760ee5bbd7cc81c49856181d7583f944
Vasilii <vbraila@list.ru>
2025-03-19 20:09:07 +0200

    added flake8

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from src.masks import get_mask_card_number, get_mask_account  # Импортируем функции из модуля masks
 
+
 def main():
     """Основная функция программы."""
     card_number = input("Введите номер карты: ")  # Запрашиваем у пользователя номер карты
@@ -10,6 +11,7 @@ def main():
     account_number = input("Введите номер счета: ")  # Запрашиваем у пользователя номер счета
     masked_account = get_mask_account(account_number)  # Маскируем номер счета с помощью функции
     print(f"Маскированный номер счета: {masked_account}")  # Выводим маскированный номер счета
+
 
 if __name__ == "__main__":
     main()  # Вызываем функцию main, если скрипт запущен напрямую
